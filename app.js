9d8381c05e90cafe91aeb1d047a30e169de6c4d9
Alex Steele <alex.steele@enablis.co>
2025-12-12 11:59:02 +0000

    fixing routing issue

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-app.use("/api", routes);
+// Register games routes first to avoid conflict with /api/:id
 app.use("/api/games", gamesRoutes);
+app.use("/api", routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
