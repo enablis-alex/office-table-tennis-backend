d86c1b2f783878a6acf3cb466d61c0192bb4a9c7
Alex Steele <alex.steele@enablis.co>
2025-12-12 11:51:12 +0000

    fixing be

--- models/Game.js
+++ models/Game.js
@@ -14,59 +14,47 @@ Game.init(
     player1Id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
-      },
-      field: "player1_id",
+      columnName: "player1_id",
     },
     player2Id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
-      },
-      field: "player2_id",
+      columnName: "player2_id",
     },
     winnerId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
-      },
-      field: "winner_id",
+      columnName: "winner_id",
     },
     player1EloBefore: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player1_elo_before",
+      columnName: "player1_elo_before",
     },
     player2EloBefore: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player2_elo_before",
+      columnName: "player2_elo_before",
     },
     player1EloAfter: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player1_elo_after",
+      columnName: "player1_elo_after",
     },
     player2EloAfter: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player2_elo_after",
+      columnName: "player2_elo_after",
     },
     player1EloChange: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player1_elo_change",
+      columnName: "player1_elo_change",
     },
     player2EloChange: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: "player2_elo_change",
+      columnName: "player2_elo_change",
     },
   },
   {
--- models/associations.js
+++ models/associations.js
@@ -9,24 +9,6 @@ const models = {
 };
 
 // Set up associations
-// A user can be player1 in many games
-User.hasMany(Game, {
-  foreignKey: "player1Id",
-  as: "gamesAsPlayer1",
-});
-
-// A user can be player2 in many games
-User.hasMany(Game, {
-  foreignKey: "player2Id",
-  as: "gamesAsPlayer2",
-});
-
-// A user can be the winner in many games
-User.hasMany(Game, {
-  foreignKey: "winnerId",
-  as: "gamesWon",
-});
-
 // A game belongs to player1
 Game.belongsTo(User, {
   foreignKey: "player1Id",
